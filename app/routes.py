17b4cd5d67cf884c501055a7228c2d7471839443
Daniel <homerion@gmail.com>
2018-06-25 11:51:53 -0400

    Flask sample project

---
+++ app/routes.py
@@ -0,0 +1,26 @@
+from app import app
+from flask import render_template, redirect,url_for
+
+
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Daniel'}
+    num = 2+2
+    return render_template('index.html', user=user,num=num,title='Home Page')
+
+@app.route('/posts')
+def posts():
+    posts=[
+    'This is post #1',
+    'This is post #2',
+    'We\'re looping through these posts.',
+    'This is Flask!'
+    ]
+    return render_template('posts.html',title='Posts',posts=posts)
+
+@app.route('/redirect')
+def goaway():
+    return redirect(url_for('index'))
