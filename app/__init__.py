0db2f642c64b051a920f8c69351c99ca2ad8c196
Daniel <homerion@gmail.com>
2018-06-26 13:35:26 -0400

    Starting to model a database using flask

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from config import Config
 from flask_bootstrap import Bootstrap
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app=Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
 
 from app import routes
-
-
 
 bootstrap=Bootstrap(app)
--- config.py
+++ config.py
@@ -1,7 +1,14 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY=os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = \
+    os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
 #you will never guess should be a huge psudorandom string
 #DO NOT USE THE SECRET KEY ON GITHUB
 
